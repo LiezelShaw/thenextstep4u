f03ffb84083a8ad14e763d1995fbb49cb5db2809
Liezel Shaw <liezelshaw@gmail.com>
2020-09-26 13:58:38 +0200

    Initial commit

---
+++ config-server/src/test/java/com/thenextstep4u/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thenextstep4u.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
